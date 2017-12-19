d2e56c161da8dbf818c4eb7a9dc43e0229f80741
kurotkin <kurotkin@users.noreply.github.com>
2017-12-19 23:09:05 +0300

    implements hibernate for Product

---
+++ src/main/java/com/kurotkin/manufacturer_data/dao/hibernate/HManufacturerDAO.java
@@ -0,0 +1,33 @@
+package com.kurotkin.manufacturer_data.dao.hibernate;
+
+import com.kurotkin.manufacturer_data.dao.ManufacturerDAO;
+import com.kurotkin.manufacturer_data.model.Manufacturer;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class HManufacturerDAO implements ManufacturerDAO {
+    public Manufacturer getById(Long aLong) throws SQLException {
+        return null;
+    }
+
+    public Manufacturer getByName(String name) throws SQLException {
+        return null;
+    }
+
+    public List<Manufacturer> getAll() throws SQLException {
+        return null;
+    }
+
+    public Long save(Manufacturer manufacturer) throws SQLException {
+        return null;
+    }
+
+    public void update(Manufacturer manufacturer) throws SQLException {
+
+    }
+
+    public void delete(Manufacturer manufacturer) throws SQLException {
+
+    }
+}
---
+++ src/main/java/com/kurotkin/manufacturer_data/dao/hibernate/HProductDAO.java
@@ -0,0 +1,92 @@
+package com.kurotkin.manufacturer_data.dao.hibernate;
+
+import com.kurotkin.manufacturer_data.dao.ProductDAO;
+import com.kurotkin.manufacturer_data.model.Product;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class HProductDAO implements ProductDAO {
+
+    private SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+
+    public Product getById(Long id) throws SQLException {
+        Product product = null;
+        try (Session session = this.sessionFactory.openSession()){
+            product = session.get(Product.class, id);
+            session.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return product;
+    }
+
+    public Product getByName(String name) throws SQLException {
+        Product product = null;
+        try (Session session = this.sessionFactory.openSession()) {
+            String query = "select c from Product c where c.name like :name";
+            List<Product> products = session.createQuery(query).setParameter("name", name).list();
+            if (products.size() != 0) {
+                product = products.get(0);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return product;
+    }
+
+    public List<Product> getAll() throws SQLException {
+        List<Product> result = null;
+        try (Session session = this.sessionFactory.openSession()) {
+            Query query = session.createQuery("from Product");
+            result = query.list();
+            session.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public Long save(Product product) throws SQLException {
+        Long id = null;
+        try (Session session = this.sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            id = (Long) session.save(product);
+            transaction.commit();
+            session.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return id;
+    }
+
+    public void update(Product val) throws SQLException {
+        try (Session session = this.sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            Product product = session.get(Product.class, val.getId());
+            product.setName(val.getName());
+            session.update(product);
+            transaction.commit();
+            session.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void delete(Product val) throws SQLException {
+        try (Session session = this.sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            Product product = session.get(Product.class, val.getId());
+            session.delete(product);
+            transaction.commit();
+            session.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
