018377cd1df5ab2fcd68e2b197280610e72be977
kurotkin <kurotkin@users.noreply.github.com>
2017-12-21 17:06:21 +0300

    auto shema ix bags

--- src/main/java/com/kurotkin/manufacturer_data/Main.java
+++ src/main/java/com/kurotkin/manufacturer_data/Main.java
@@ -3,12 +3,7 @@ package com.kurotkin.manufacturer_data;
 import com.kurotkin.manufacturer_data.dao.hibernate.HProductDAO;
 import com.kurotkin.manufacturer_data.model.Manufacturer;
 import com.kurotkin.manufacturer_data.model.Product;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.boot.registry.internal.StandardServiceRegistryImpl;
-import org.hibernate.boot.spi.MetadataImplementor;
-import org.hibernate.service.ServiceRegistry;
-import org.hibernate.tool.hbm2ddl.SchemaExport;
+
 
 import java.math.BigDecimal;
 
@@ -20,18 +15,5 @@ public class Main {
         productDAO.save(product);
     }
 
-    private static void init() {
-        ServiceRegistry serviceRegistry = buildCfg();
-        MetadataImplementor metadata = (MetadataImplementor) new MetadataSources(serviceRegistry).buildMetadata();
-        SchemaExport schemaExport = new SchemaExport(metadata);
-        schemaExport.setOutputFile("init.sql");
-        schemaExport.create(true, true);
-        ( (StandardServiceRegistryImpl) serviceRegistry ).destroy();
-    }
 
-    public static StandardServiceRegistryImpl buildCfg() {
-        return (StandardServiceRegistryImpl) new StandardServiceRegistryBuilder()
-                .configure("hibernate.cfg.xml")
-                .build();
-    }
 }
