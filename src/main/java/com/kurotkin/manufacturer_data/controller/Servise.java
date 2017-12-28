42b0e7601ba959772aa9aadc860d3803ab58954c
kurotkin <kurotkin@users.noreply.github.com>
2017-12-28 17:01:58 +0300

    fix manufacturer class

--- src/main/java/com/kurotkin/manufacturer_data/controller/Servise.java
+++ src/main/java/com/kurotkin/manufacturer_data/controller/Servise.java
@@ -1,6 +1,7 @@
 package com.kurotkin.manufacturer_data.controller;
 
 import com.kurotkin.manufacturer_data.dao.hibernate.HProductDAO;
+import com.kurotkin.manufacturer_data.model.Manufacturer;
 import com.kurotkin.manufacturer_data.model.Product;
 
 import javax.servlet.ServletException;
@@ -22,19 +23,27 @@ public class Servise extends HttpServlet{
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
 
-        PrintWriter p = response.getWriter();
+        final PrintWriter p = response.getWriter();
         p.println("<!DOCTYPE html>" + "<html><head><title>" + mess + "</title></head><body>");
-        p.println("<h1>" + mess + "<h1>");
+        p.println("<h1>" + mess + "</h1>");
 
         HProductDAO productDAO = new HProductDAO();
         List<Product> products = productDAO.getAll();
 
-        products.stream().forEach(  product -> {
-            p.println("ID: " + product.getId() + "<br/>");
-            p.println("Name: " + product.getName() + "<br/>");
-            p.println("Price: " + product.getPrice() + "<br/>");
-            p.println("Manufacturer: " + product.getManufacturer().getName() + "<br/>");
-        });
+        for(int i = 0; i < products.size(); i++){
+            p.println("ID: " + products.get(i).getId() + "<br>");
+            p.println("Name: " + products.get(i).getName() + "<br>");
+            p.println("Price: " + products.get(i).getPrice() + "<br>");
+            Manufacturer manufacturer = products.get(i).getManufacturer();
+            if(manufacturer == null){
+                p.println("Manufacturer: " + "null" + "<br/>");
+            } else {
+                p.println("Manufacturer: " + manufacturer.getId() + "<br/>");
+                p.println("_____________ " + manufacturer.getName() + "<br/>");
+            }
+
+            p.println("<br>");
+        }
 
         p.println("</body></html>");
     }
