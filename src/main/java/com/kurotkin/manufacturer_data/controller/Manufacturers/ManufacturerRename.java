42a7fd0a1193bed41991ba395a12e9aeb70e119e
kurotkin <kurotkin@users.noreply.github.com>
2018-01-17 23:33:52 +0300

    added manufacturer rename

---
+++ src/main/java/com/kurotkin/manufacturer_data/controller/Manufacturers/ManufacturerRename.java
@@ -0,0 +1,25 @@
+package com.kurotkin.manufacturer_data.controller.Manufacturers;
+
+import com.kurotkin.manufacturer_data.dao.hibernate.HManufacturerDAO;
+import com.kurotkin.manufacturer_data.model.Manufacturer;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ManufacturerRename extends HttpServlet {
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HManufacturerDAO manufacturerDAO = new HManufacturerDAO();
+        String idString = request.getParameter("rename");
+        long id = Long.parseLong(idString);
+        String newName = request.getParameter("newname");
+
+        Manufacturer manufacturer = new Manufacturer().withID(id).withName(newName);
+        manufacturerDAO.update(manufacturer);
+
+        new ManufacturersPage().doGet(request, response);
+    }
+}
